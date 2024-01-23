08834c82c3d4239d888aa9673661ecd1b756bbc3
anousonefs <aworlakoumman@gmail.com>
2024-01-23 15:49:21 +0700

    implement tls in client and server

--- client/main.go
+++ client/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 
 	"client/services"
 
 	"google.golang.org/grpc"
+
+	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/credentials/insecure"
 
 	status "google.golang.org/grpc/status"
@@ -14,15 +17,30 @@ import (
 
 func main() {
 
-	creds := insecure.NewCredentials()
-	cc, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(creds))
+	var cc *grpc.ClientConn
+	var err error
+	var creds credentials.TransportCredentials
+
+	host := flag.String("host", "localhost:50051", "gRPC server host")
+	tls := flag.Bool("tls", false, "Connection uses TLS if true, else plain")
+	flag.Parse()
+	if *tls {
+		certFile := "../tls/ca.crt"
+		creds, err = credentials.NewClientTLSFromFile(certFile, "")
+	} else {
+		creds = insecure.NewCredentials()
+	}
+
+	cc, err = grpc.Dial(*host, grpc.WithTransportCredentials(creds))
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	defer cc.Close()
+
 	calculcatorClient := services.NewCalculatorClient(cc)
 	calculatorService := services.NewCalculatorService(calculcatorClient)
-	err = calculatorService.Hello("")
+	err = calculatorService.Hello("anousone")
 	/* err = calculatorService.Fibonacci(10) */
 	/* err = calculatorService.Average(1, 2, 3, 4, 5, 6, 7, 8, 9, 10) */
 	/* err = calculatorService.Sum(1, 2, 3, 4, 5, 6) */
--- server/main.go
+++ server/main.go
@@ -1,17 +1,34 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"grpc-learn/services"
 	"log"
 	"net"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/reflection"
 )
 
 func main() {
-	s := grpc.NewServer()
+
+	var s *grpc.Server
+	tls := flag.Bool("tls", false, "Connection uses TLS if true, else plain")
+	flag.Parse()
+	if *tls {
+		certFile := "../tls/server.crt"
+		keyFile := "../tls/server.pem"
+		creds, err := credentials.NewServerTLSFromFile(certFile, keyFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		s = grpc.NewServer(grpc.Creds(creds))
+	} else {
+		s = grpc.NewServer()
+	}
+
 	services.RegisterCalculatorServer(s, services.NewCalculatorServer())
 
 	reflection.Register(s)
@@ -20,7 +37,12 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("Listening on :50051")
+	fmt.Print("Listening on :50051")
+	if *tls {
+		fmt.Println(" with TLS")
+	} else {
+		fmt.Println()
+	}
 	if err := s.Serve(listener); err != nil {
 		log.Fatal(err)
 	}
